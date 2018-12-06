d34af0d28e385205a6842f98f8b916208acd3d40
a503-17 <1>
2018-12-06 10:34:30 +0900

    upload

--- 1102clock/ViewController.swift
+++ 1102clock/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //타이머가 수행할 메소드
+    @objc func timerProc(){
+        //현재 날짜 가져오기
+        let date = Date()
+        //날짜를 문자열로 변경하기 위한 클래스의 객체 만들기
+        let fomatter = DateFormatter()
+        //문자열 형식 만들기
+        fomatter.dateFormat = "yyyy-MM-dd ccc hh:mm:ss"
+        //레이블에 날짜를 문자열로 변경해서 출력
+        label.text = fomatter.string(from: date)
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Timer.scheduledTimer(timeInterval: 1,
+                             target:self,
+                             selector:#selector(timerProc),
+                             userInfo:nil,
+                             repeats:true)
+        
     }
-
-
+    
+    
 }
+
 
---
+++ 1106TouchEvent/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  1106Touch
+//
+//  Created by 502 on 2018. 11. 6..
+//  Copyright © 2018년 502. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblCount: UILabel!
+    @IBOutlet weak var lblState: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //터치가 시작되면 호출되는 메소드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //터치를 1개만 하면 touches에서 첫번째 데이터를 꺼내면 됩니다.
+        let touch = touches.first
+        lblCount.text = "\(touch!.tapCount)번 탭"
+        lblState.text = "터치를 한 상태입니다."
+    }
+    
+    //터치가 이동할 때 호출되는 메소드
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lblState.text = "터치가 이동 중 입니다."
+        //발생한 터치 가져오기
+        let touch = touches.first
+        //imageView 에서 터치가 이동 중이라면
+        //터치의 좌표를 imageView의 중앙점으로 설정
+        if touch!.view == imageView{
+            imageView.center = touch!.location(in:self.view)
+        }
+    }
+    
+    //터치가 종료되었을 때 호출되는 메소드
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lblState.text = "터치가 종료 되었습니다."
+    }
+
+
+
+}
+
